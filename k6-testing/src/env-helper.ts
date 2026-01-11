91bdedf798af2fe24da66ebfe6b01fea89ed4a30
MoazHassan2022 <moaaz.mohamed@dafagate.com>
2026-01-11 14:08:31 +0200

    add nfts testing

--- k6-testing/src/env-helper.ts
+++ k6-testing/src/env-helper.ts
@@ -1,7 +1,6 @@
 export function loadEnv(): Record<string, string> {
   const path = '../.env';
   const envFile = open(path);
-  console.log(`Loading environment variables from ${envFile}`);
   const env: Record<string, string> = {};
 
   envFile.split("\n").forEach((line) => {
@@ -12,8 +11,6 @@ export function loadEnv(): Record<string, string> {
     const value = rest.join("=").trim();
     env[key.trim()] = value;
   });
-
-  console.log(`Loaded environment variables: ${JSON.stringify(env)}`);
 
   return env;
 }
--- k6-testing/src/nfts/create-nfts.ts
+++ k6-testing/src/nfts/create-nfts.ts
@@ -4,16 +4,18 @@ import http from 'k6/http';
 import { loadEnv } from '../env-helper';
 import exec from 'k6/execution';
 
+// init stage file load
+const imageBin = open('../src/nfts/image.png', 'b');
+
 export const options: Options = {
   scenarios: {
-    createClient: {
+    createNFT: {
       executor: 'shared-iterations',
       vus: 10,
       iterations: 1000,
-      // duration: '10s',
-      exec: 'createClient',
+      exec: 'createNFT',
     },
-  }
+  },
 };
 
 const env = loadEnv();
@@ -21,37 +23,45 @@ const env = loadEnv();
 export function setup() {
   const token = env.VAULT_AUTH_TOKEN;
 
-  console.log(`Using VAULT_AUTH_TOKEN: ${token}`);
-
   if (!token) {
     throw new Error('VAULT_AUTH_TOKEN is not set in .env');
   }
 
-  return {
-    token,
-  };
+  return { token };
 }
 
-export function createClient(data: { token: string }) {
+export function createNFT(data: { token: string }) {
   const index = exec.scenario.iterationInTest + 1;
+  const clientId = `client${index}`;
 
-  const payload = JSON.stringify({
-    id: `client${index}`,
-  });
+  const formData = {
+    networkId: '29',
+    name: 'NFT',
+    symbol: 'NFT',
+    ownerClientId: clientId,
+
+    image: http.file(
+      imageBin,
+      'image.png',
+      'image/png'
+    ),
+  };
 
   const headers = {
-    'Content-Type': 'application/json',
     'X-ACCESS-TOKEN': data.token,
   };
 
   const res = http.post(
-    'http://rox.localhost:4000/api/integration/clients/create',
-    payload,
+    'http://rox.localhost:4000/api/integration/tokenization/nft',
+    formData,
     { headers }
   );
 
+  console.log('Response Body:', res.body);
+  console.log('Status Code:', res.status);
+
   check(res, {
-    'createClient status is 201': (r) => r.status === 201,
+    'createNFT status is 201': (r) => r.status === 201,
   });
 
   sleep(0.001);
